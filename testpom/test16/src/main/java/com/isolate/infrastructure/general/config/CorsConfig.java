c43b9ddaf599645de7a7bf3a5de2b405e9a451b3
isolated <15071340963@163.com>
2021-08-03 10:30:17 +0800

    add allow origin

---
+++ testpom/test16/src/main/java/com/isolate/infrastructure/general/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.isolate.infrastructure.general.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @description: some desc
+ * @author: isolate
+ * @email: 15071340963@163.com
+ * @date: 2021/8/3 9:43
+ */
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
