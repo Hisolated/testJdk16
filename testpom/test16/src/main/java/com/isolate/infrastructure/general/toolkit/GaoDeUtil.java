9d003b0009a951d2858845801fdf6ac3a1a2b660
isolated <15071340963@163.com>
2021-08-19 09:12:07 +0800

    fixed

--- testpom/test16/src/main/java/com/isolate/infrastructure/general/toolkit/GaoDeUtil.java
+++ testpom/test16/src/main/java/com/isolate/infrastructure/general/toolkit/GaoDeUtil.java
@@ -36,8 +36,9 @@ public class GaoDeUtil {
     private static RestTemplate restTemplate;
 
     /**
-     * 获取精确的地址位置
-     * @param ip 需要查询的ip
+     * 获取精确的地址位置（此方法误差2km）
+     *
+     * @param ip   需要查询的ip
      * @param type 值为 4 或 6，4 表示 IPv4，6 表示 IPv6
      * @return 精确的地理地址
      */
@@ -45,32 +46,32 @@ public class GaoDeUtil {
         /*
          *  1. 先获取该ip所对应的经纬度
          */
-        Map<String,Object> param = new HashMap<>(4);
-        param.put("key",GAO_DE_KEY);
-        param.put("type",type);
-        param.put("ip",ip);
+        Map<String, Object> param = new HashMap<>(4);
+        param.put("key", GAO_DE_KEY);
+        param.put("type", type);
+        param.put("ip", ip);
         String jsonStr = RestTemplateUtil.getForObject(IP_URL, param);
 
         ObjectMapper mapper = new ObjectMapper();
 
         String location = mapper.readTree(jsonStr).path("location").asText();
 
-
-        Map<String,Object> param2 = new HashMap<>();
-        param2.put("key",GAO_DE_KEY);
-        param2.put("location",location);
-        param2.put("radius",200);
-        param2.put("extensions","all");
-        param2.put("batch",false);
+        /*
+         * 2. 再通过经纬度获取精确的ip地址
+         */
+        Map<String, Object> param2 = new HashMap<>();
+        param2.put("key", GAO_DE_KEY);
+        param2.put("location", location);
+        param2.put("radius", 200);
+        param2.put("extensions", "all");
+        param2.put("batch", false);
 
         String jsonStr2 = RestTemplateUtil.getForObject(REGEO_URL, param2);
 
-        ObjectMapper mapper2 = new ObjectMapper();
-        JsonNode regeocodes = mapper2.readTree(jsonStr2).path("regeocode");
+        JsonNode regeoCodes = mapper.readTree(jsonStr2).path("regeocode");
 
-        String formatted_address = regeocodes.path("formatted_address").asText();
-        return formatted_address;
-
+        String formattedAddress = regeoCodes.path("formatted_address").asText();
+        return formattedAddress;
     }
 
 }
