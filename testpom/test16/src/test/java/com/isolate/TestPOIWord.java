ed00ac6786342a2fd488ab54e5e312e153f59f71
hujianqiang <742559187@qq.com>
2023-02-14 13:39:16 +0800

    解析word中内容控件数据

--- testpom/test16/src/test/java/com/isolate/TestPOIWord.java
+++ testpom/test16/src/test/java/com/isolate/TestPOIWord.java
@@ -9,6 +9,7 @@ import org.apache.poi.xwpf.usermodel.*;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.*;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -71,7 +72,7 @@ public class TestPOIWord {
             FileInputStream in = new FileInputStream(filePath);//载入文档
             // 处理docx格式 即office2007以后版本
             if(filePath.toLowerCase().endsWith("docx")){
-                //word 2007 图片不会被读取， 表格中的数据会被放在字符串的最后
+
                 XWPFDocument xwpf = new XWPFDocument(in);//得到word文档的信息
                 Iterator<XWPFTable> it = xwpf.getTablesIterator();//得到word中的表格
                 List<XWPFParagraph> paragraphList = xwpf.getParagraphs();
@@ -94,8 +95,21 @@ public class TestPOIWord {
                         System.err.println("第"+i+"行：");
                         //读取每一列数据
                         List<XWPFTableCell> cells = row.getTableCells();
+                        List<CTSdtCell> tblSdtList = row.getCtRow().getSdtList();
+
+                        if (tblSdtList != null && tblSdtList.size() > 0) {
+                            for (CTSdtCell ctSdtCell : tblSdtList) {
+                                CTSdtPr sdtPr = ctSdtCell.getSdtPr();
+//                                printSdtPrContent(sdtPr);
+                                CTSdtContentCell sdtContent = ctSdtCell.getSdtContent();
+                                printSdtContentCell(sdtContent);
+                            }
+                        }
+
+
                         for (int j = 0; j < cells.size(); j++) {
                             XWPFTableCell cell = cells.get(j);
+                            List<IBodyElement> bodyElements = cell.getBodyElements();
                             List<XWPFParagraph> paragraphs = cell.getParagraphs();
                             for (XWPFParagraph paragraph : paragraphs) {
                                 List<String> imageBundleList = XWPFUtils.readImageInParagraph(paragraph);
@@ -198,5 +212,163 @@ public class TestPOIWord {
         }
 //        System.out.println(JSON.toJSONString(resultList, true));
     }
+
+
+
+    //无法获取更多的内容(如文本类型,是否可编辑)
+    public void printXWPFSDTContent(XWPFSDT sdt) {
+        StringBuffer sb = new StringBuffer();
+        sb.append(" 标记:").append(sdt.getTag());
+        sb.append(" 标题:").append(sdt.getTitle());
+        ISDTContent content = sdt.getContent();
+        sb.append(" 内容:").append(content.getText());
+        System.out.println(sb.toString());
+    }
+
+    public void printParaListContent(List<XWPFParagraph> paraList) {
+        if (paraList == null || paraList.size() == 0) {
+            return;
+        }
+        for (XWPFParagraph para : paraList) {
+            List<CTSdtRun> sdtList = para.getCTP().getSdtList();
+            if (sdtList == null || sdtList.size() == 0) {
+                continue;
+            }
+            for (CTSdtRun sdtRun : sdtList) {
+                CTSdtPr sdtPr = sdtRun.getSdtPr();
+//                printSdtPrContent(sdtPr);
+                CTSdtContentRun sdtContent = sdtRun.getSdtContent();
+                printSdtContent(sdtContent);
+            }
+        }
+    }
+//
+//    // 解析样式,区分纯文本和格式文本
+//    public void printSdtPrContent(CTSdtPr sdtPr) {
+//        if (sdtPr == null) {
+//            return;
+//        }
+//        StringBuffer sb = new StringBuffer();
+//        List<CTSdtText> textList = sdtPr.getTextList();
+//        if (textList != null && textList.size() > 0) {
+//            sb.append(" 内容控件类型:").append("纯文本");
+//            CTSdtText sdtText = textList.get(0);
+//            if (sdtText.getMultiLine() != null) {
+//                int mulType = sdtText.getMultiLine().intValue();
+//                if (mulType == 1 || mulType == 3 || mulType == 6) {
+//                    sb.append(" 是否允许回车:").append("是");
+//                }
+//            }
+//        } else {
+//            sb.append(" 内容控件类型:").append("格式文本");
+//        }
+//        List<CTDecimalNumber> idList = sdtPr.getIdList();
+//        if (idList != null && idList.size() > 0) {
+//            sb.append(" ID:").append(idList.get(0).getVal());
+//        }
+//        List<CTString> aliasList = sdtPr.getAliasList();
+//        if (aliasList != null && aliasList.size() > 0) {
+//            sb.append(" 标题:").append(aliasList.get(0).getVal());
+//        }
+//        List<CTString> tagList = sdtPr.getTagList();
+//        if (tagList != null && tagList.size() > 0) {
+//            sb.append(" 标记:").append(tagList.get(0).getVal());
+//        }
+//        List<CTLock> lockList = sdtPr.getLockList();
+//        if (lockList != null && lockList.size() > 0) {
+//            CTLock ctLock = lockList.get(0);
+//            int lockType = ctLock.getVal().intValue();
+//            switch (lockType) {
+//                case 1:
+//                    sb.append(" 锁定方式:").append("无法删除内容控件");
+//                    break;
+//                case 2:
+//                    sb.append(" 锁定方式:").append("无法编辑内容");
+//                    break;
+//                case 4:
+//                    sb.append(" 锁定方式:").append("无法删除内容控件，无法编辑内容");
+//                    break;
+//                default:
+//                    sb.append(" 锁定方式:").append(ctLock.getVal());
+//                    break;
+//            }
+//        }
+//        List<CTOnOff> tempList = sdtPr.getTemporaryList();
+//        if (tempList != null && tempList.size() > 0) {
+//            if (tempList.get(0).getVal() != null) {
+//                int isOn = tempList.get(0).getVal().intValue();
+//                if (isOn == 1 || isOn == 3 || isOn == 6) {
+//                    sb.append(" 替换后是否删除内容控件:").append("是");
+//                }
+//            } else {
+//                sb.append(" 替换后是否删除内容控件:").append("是");
+//            }
+//        }
+//        List<CTRPr> rprList = sdtPr.getRPrList();
+//        if (rprList != null && rprList.size() > 0) {
+//            CTRPr rpr = rprList.get(0);
+//            CTString rprStyle = rpr.getRStyle();
+//            if (rprStyle != null) {
+//                sb.append(" 样式名称:").append(rprStyle.getVal());
+//            }
+//        }
+//        System.out.println(sb.toString());
+//    }
+
+    // 段落内内容控件
+    public void printSdtContent(CTSdtContentRun sdtContentRun) {
+        if (sdtContentRun == null) {
+            return;
+        }
+        StringBuffer sb = new StringBuffer();
+        List<CTR> ctrList = sdtContentRun.getRList();
+        sb.append(getCTRContent(ctrList));
+        System.out.println("内容:" + sb.toString());
+    }
+
+    // 表格内内容控件
+    public void printSdtContentCell(CTSdtContentCell sdtContentCell) {
+        if (sdtContentCell == null) {
+            return;
+        }
+        StringBuffer sb = new StringBuffer();
+        List<CTTc> cttcList = sdtContentCell.getTcList();
+        sb.append(getCTTcContent(cttcList));
+        System.out.println("内容:" + sb.toString());
+    }
+
+    // 段落
+    public String getCTRContent(List<CTR> ctrList) {
+        StringBuffer sb = new StringBuffer();
+        if (ctrList != null && ctrList.size() > 0) {
+            for (CTR ctr : ctrList) {
+                List<CTText> tList = ctr.getTList();
+                if (tList != null && tList.size() > 0) {
+                    for (CTText ctText : tList) {
+                        sb.append(ctText.getStringValue());
+                    }
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    // 表格
+    public String getCTTcContent(List<CTTc> cttcList) {
+        StringBuffer sb = new StringBuffer();
+        if (cttcList != null && cttcList.size() > 0) {
+            for (CTTc cttc : cttcList) {
+                List<CTP> pList = cttc.getPList();
+                if (pList != null && pList.size() > 0) {
+                    for (CTP ctp : pList) {
+                        List<CTR> rList = ctp.getRList();
+                        sb.append(getCTRContent(rList));
+                    }
+                }
+            }
+        }
+        return sb.toString();
+    }
+
 
 }
